44701749ee9c946dea0cab07ec088e0519daa2ec
everton leonel <everton_leonel@hotmail.com>
2024-04-15 21:37:01 -0400

    add: erros usecase

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,8 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
 import { RegisterUseCase } from "@/use-cases/register";
-import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-existis-error";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -21,8 +22,14 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       email,
       password,
     });
-  } catch {
-    reply.status(409).send();
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      reply.status(409).send({
+        message: err.message,
+      });
+    }
+
+    return reply.status(500).send();
   }
 
   return reply.status(201).send();
